1a0a1e14297f2af063fe77a916fcae2355ffc2f7
mohamad sabah jasiem <63969700+mohamadsabah1999@users.noreply.github.com>
2021-11-09 03:42:00 +0300

    add switch button and coach table with database

--- index.php
+++ index.php
@@ -2,30 +2,82 @@
 require_once('connection.php');
 
 //Code for Registration 
-if(isset($_POST['signup']))
+if(!isset($_POST['coach']))
 {
-	$fname=$_POST['fname'];
-	$lname=$_POST['lname'];
-	$email=$_POST['email'];
-	$password=$_POST['password'];
-	$contact=$_POST['contact'];
-	$enc_password=$password;
-$sql=mysqli_query($con,"select id from users where email='$email'");
-$row=mysqli_num_rows($sql);
-if($row>0)
-{
-	echo "<script>alert('Email id already exist with another account. Please try with other email id');</script>";
-} else{
-	$msg=mysqli_query($con,"insert into users(fname,lname,email,password,contactno) values('$fname','$lname','$email','$enc_password','$contact')");
+	if(isset($_POST['signup']))
+	{
+		$fname=$_POST['fname'];
+		$lname=$_POST['lname'];
+		$email=$_POST['email'];
+		$password=$_POST['password'];
+		$contact=$_POST['contact'];
+		$enc_password=$password;
+	$sql=mysqli_query($con,"select id from users where email='$email'");
+	$row=mysqli_num_rows($sql);
+	if($row>0)
+	{
+		echo "<script>alert('Email id already exist with another account. Please try with other email id');</script>";
+	} else{
+		$msg=mysqli_query($con,"insert into users(fname,lname,email,password,contactno) values('$fname','$lname','$email','$enc_password','$contact')");
+	
+	if($msg)
+	{
+		echo "<script>alert('Register successfully');</script>";
+	}
+	}
+	}
+}
+elseif(isset($_POST['coach'])) 
+		
+	{
 
-if($msg)
-{
-	echo "<script>alert('Register successfully');</script>";
-}
-}
-}
+		if(isset($_POST['signup']))
+		{
+			$fname=$_POST['fname'];
+			$lname=$_POST['lname'];
+			$email=$_POST['email'];
+			$password=$_POST['password'];
+			$contact=$_POST['contact'];
+			$enc_password=$password;
+		$sql=mysqli_query($con,"select id from coach where email='$email'");
+		$row=mysqli_num_rows($sql);
+		if($row>0)
+		{
+			echo "<script>alert('Email id already exist with another account. Please try with other email id');</script>";
+		} else{
+			$msg=mysqli_query($con,"insert into coach(fname,lname,email,password,contactno) values('$fname','$lname','$email','$enc_password','$contact')");
+		
+		if($msg)
+		{
+			echo "<script>alert('Register successfully');</script>";
+		}
+		}
+		}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	}
+
 
 // Code for login 
+
+
 if(isset($_POST['login']))
 {
 $password=$_POST['password'];
@@ -160,8 +212,18 @@ echo "<script>alert('Email not register with us');</script>";
 
 									<input type="submit" name="signup"  value="Sign Up" >
 			                        </div>
+										
 									<div class="clear"> </div>
 								</div>
+								<div><p style="display:contents;">coach ?</p></div><br>	
+								<label class="switch">
+									<input type="checkbox" name="coach">
+									<span class="slider round"></span>
+									
+									</label>
+									
+									
+									
 							</form>
 
 						</div>
