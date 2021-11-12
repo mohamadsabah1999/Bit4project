c1d180219a0784a9664821ce3ea39c25c405dba7
mohamad sabah jasiem <63969700+mohamadsabah1999@users.noreply.github.com>
2021-11-12 20:38:32 +0300

    edit with image button

--- coach/manage-users.php
+++ coach/manage-users.php
@@ -127,8 +127,8 @@ echo "<script>alert('Data deleted');</script>";
                   </div>
               </div>
 		</section>
-      </section
-  ></section>
+      </section>
+    </section>
     <script src="assets/js/jquery.js"></script>
     <script src="assets/js/bootstrap.min.js"></script>
     <script class="include" type="text/javascript" src="assets/js/jquery.dcjqaccordion.2.7.js"></script>
