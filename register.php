607c0bc12f321221cb7d47a584d41ecafc276db8
yo12sif <yosifhtml@gmail.com>
2021-11-13 23:23:16 +0300

    new2

---
+++ register.php
@@ -0,0 +1,66 @@
+<?php 
+session_start();
+
+	include("connection.php");
+	include("functions.php");
+
+
+	if($_SERVER['REQUEST_METHOD'] == "POST")
+	{
+		//something was posted
+		$user_name = $_POST['user_name'];
+		$password = $_POST['password'];
+		$password=md5($password);
+
+		if(!empty($user_name) && !empty($password) && !is_numeric($user_name))
+		{
+
+			//save to database
+		
+			$query = "insert into users (user_name,password) values ('$user_name','$password')";
+
+			mysqli_query($con, $query);
+
+			header("Location: login.php");
+			die;
+		}else
+		{
+			echo "Please enter some valid information!";
+		}
+	}
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<link rel="stylesheet" href="style.css">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+  <meta charset="UTF-8">
+ <meta name="viewport" content="width=device-width, initial-scale=1.0">
+ <title>Register_form</title>
+</head>
+<body>
+<form method="post"> 
+   <div class="container">
+    <h1>Register</h1>
+    <p>Please fill in this form to create an account.</p>
+    <hr>
+    <div>
+    <label ><b>Username</b></label>
+    <input type="text" placeholder="Enter your user name" name="user_name"  required>
+    </div> <div>
+    <label ><b>Password</b></label>
+    <input type="password" placeholder="Enter Password" name="password"   required>
+    </div>
+    <hr>
+    <p>By creating an account you agree to our Terms & Privacy.</p>
+    <button type="submit" class="registerbtn" value="Signup">Register</button>
+    <div class="container signin">
+    <p>Already have an account? <a href="login.php">Sign in</a>.</p>
+  </div>
+  </div>
+  
+</form> 
+</body>
+</html>
---
+++ welcome.php
@@ -0,0 +1,96 @@
+<?php
+session_start();
+if (strlen($_SESSION['id']==0)) {
+  header('location:logout.php');
+  } else{
+	
+?><!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+
+    <title>Welcome </title>
+  
+    
+
+    
+</head>
+<body>
+<nav class="navbar navbar-expand-lg navbar navbar-dark bg-dark">
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+    <a class="navbar-brand" href="#">Gym trainer</a>
+    <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+      <li class="nav-item active">
+        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link" href="#"><?php echo $_SESSION['name'];?></a>
+      </li>
+      <li class="nav-item">
+      <a class="nav-link" href="logout.php">Logout</a>
+      </li>
+    </ul>
+   
+  </div>
+</nav>
+
+<form name="form1" action="" method="post" enctype="multipart/form-data">
+<table>
+<tr>
+<td><input type="submit" name="submit2" value="display"></td>
+</tr>
+</table>
+</form>
+<?php
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'loginsystem');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+
+
+
+if(isset($_POST["submit2"]))
+{
+   $res=mysqli_query($con,"select * from coach");
+   echo "<table>";
+   echo "<tr>";
+   
+   while($row=mysqli_fetch_array($res))
+   {
+   echo "<td>"; 
+   echo '<img src="data:coach/images;base64,'.base64_encode($row['image'] ).'" height="200" width="200"/>';
+   echo "<br>";
+   ?><a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a> <?php
+   echo "</td>";
+   } 
+   echo "</tr>";
+   
+   echo "</table>";
+  }
+?>
+
+</body>
+
+</html>
+<?php } ?>
