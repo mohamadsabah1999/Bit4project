132892aff22a9fdc75aee37ea553bca052687568
mohamad sabah jasiem <63969700+mohamadsabah1999@users.noreply.github.com>
2021-11-13 20:37:26 +0300

    add code

--- welcome.php
+++ welcome.php
@@ -47,6 +47,49 @@ if (strlen($_SESSION['id']==0)) {
   </div>
 </nav>
 
+<form name="form1" action="" method="post" enctype="multipart/form-data">
+<table>
+<tr>
+<td><input type="submit" name="submit2" value="display"></td>
+</tr>
+</table>
+</form>
+<?php
+define('DB_SERVER','localhost');
+define('DB_USER','root');
+define('DB_PASS' ,'');
+define('DB_NAME', 'loginsystem');
+$con = mysqli_connect(DB_SERVER,DB_USER,DB_PASS,DB_NAME);
+
+// Check connection
+if (mysqli_connect_errno())
+{
+echo "Failed to connect to MySQL: " . mysqli_connect_error();
+ }
+
+
+
+
+if(isset($_POST["submit2"]))
+{
+   $res=mysqli_query($con,"select * from coach");
+   echo "<table>";
+   echo "<tr>";
+   
+   while($row=mysqli_fetch_array($res))
+   {
+   echo "<td>"; 
+   echo '<img src="data:coach/images/;base64,'.base64_encode($row['image'] ).'" height="200" width="200"/>';
+   echo "<br>";
+   ?><a href="delete.php?id=<?php echo $row["id"]; ?>">Delete</a> <?php
+   echo "</td>";
+   } 
+   echo "</tr>";
+   
+   echo "</table>";
+  }
+?>
+
 </body>
 
 </html>
